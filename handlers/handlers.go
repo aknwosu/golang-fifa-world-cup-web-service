c31fd32b94c5b00c63fe3711c38f7ac06df4ff3b
Rebecca Nwosu <akunnarebecca@gmail.com>
2020-01-22 18:01:23 +0100

    Completed module

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -15,7 +15,6 @@ func ListWinners(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "application/json")
 	year := req.URL.Query().Get("year")
 	if year == "" {
-
 		winners, err := data.ListAllJSON()
 		if err != nil {
 			res.WriteHeader(http.StatusInternalServerError)
